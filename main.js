16038ba1090b4b062b4030f13a513f27e6b55efc
Claaa00 <cmartinp0804@gmail.com>
2022-11-08 15:32:30 +0800

    fixedOverlays(DoubleClickTrigger)

---
+++ main.js
@@ -0,0 +1,244 @@
+// side menu
+
+const openMenuBar = document.getElementById("openMenuBar");
+openMenuBar.addEventListener("click", function () {
+  document.getElementById("navs").style.left = "0";
+});
+
+const closeMenuBar = document.getElementById("closeMenuBar");
+closeMenuBar.addEventListener("click", function () {
+  document.getElementById("navs").style.left = "-100%";
+});
+
+// add record overlay
+
+// const addRecordOverlay = document.getElementById("addRecordOverlay");
+// const openAddOverlayHeader = document.getElementById("openAddOverlayHeader");
+// openAddOverlayHeader.onclick = function () {
+//   if (addRecordOverlay.style.display !== "none") {
+//     addRecordOverlay.style.display = "none";
+//   } else {
+//     addRecordOverlay.style.display = "block";
+//   }
+// };
+
+function openAddOverlayHeader() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function openAddOverlay() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeAddOverlay() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeAddOverlay2() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function toggleFilterBtn() {
+  var x = document.getElementById("filterOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeFilterBtn() {
+  var x = document.getElementById("filterOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function toggleSortBtn() {
+  var x = document.getElementById("sortOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeSortBtn() {
+  var x = document.getElementById("sortOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function toggleMoreMenu() {
+  var x = document.getElementById("moreMenuActions");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function openEditOverlay() {
+  var x = document.getElementById("editRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeEditOverlay() {
+  var x = document.getElementById("editRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeEditOverlay2() {
+  var x = document.getElementById("editRecordOverlay");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// const openAddOverlay = document.getElementById("openAddOverlay");
+// openAddOverlay.onclick = function () {
+//   if (addRecordOverlay.style.display !== "none") {
+//     addRecordOverlay.style.display = "none";
+//   } else {
+//     addRecordOverlay.style.display = "block";
+//   }
+// };
+
+// // const addRecordOverlay = document.getElementById("addRecordOverlay");
+// const closeAddOverlay = document.getElementById("closeAddOverlay");
+// closeAddOverlay.onclick = function () {
+//   if (addRecordOverlay.style.display !== "none") {
+//     addRecordOverlay.style.display = "none";
+//   } else {
+//     addRecordOverlay.style.display = "block";
+//   }
+// };
+
+// const closeAddOverlay2 = document.getElementById("closeAddOverlay2");
+// closeAddOverlay2.onclick = function () {
+//   if (addRecordOverlay.style.display !== "none") {
+//     addRecordOverlay.style.display = "none";
+//   } else {
+//     addRecordOverlay.style.display = "block";
+//   }
+// };
+
+// // edit record overlay
+
+// const editRecordOverlay = document.getElementById("editRecordOverlay");
+// const openEditOverlay = document.getElementById("openEditOverlay");
+// openEditOverlay.onclick = function () {
+//   if (editRecordOverlay.style.display !== "none") {
+//     editRecordOverlay.style.display = "none";
+//   } else {
+//     editRecordOverlay.style.display = "block";
+//   }
+// };
+
+// // const addRecordOverlay = document.getElementById("addRecordOverlay");
+// const closeEditOverlay = document.getElementById("closeEditOverlay");
+// closeEditOverlay.onclick = function () {
+//   if (editRecordOverlay.style.display !== "none") {
+//     editRecordOverlay.style.display = "none";
+//   } else {
+//     editRecordOverlay.style.display = "block";
+//   }
+// };
+
+// const closeEditOverlay2 = document.getElementById("closeEditOverlay2");
+// closeEditOverlay2.onclick = function () {
+//   if (editRecordOverlay.style.display !== "none") {
+//     editRecordOverlay.style.display = "none";
+//   } else {
+//     editRecordOverlay.style.display = "block";
+//   }
+// };
+
+// // filter
+
+// const filterOverlay = document.getElementById("filterOverlay");
+// const toggleFilterBtn = document.getElementById("toggleFilterBtn");
+// toggleFilterBtn.onclick = function () {
+//   if (filterOverlay.style.display !== "none") {
+//     filterOverlay.style.display = "none";
+//   } else {
+//     filterOverlay.style.display = "block";
+//   }
+// };
+
+// // const addRecordOverlay = document.getElementById("addRecordOverlay");
+// const closeFilterBtn = document.getElementById("closeFilterBtn");
+// closeFilterBtn.onclick = function () {
+//   if (filterOverlay.style.display === "none") {
+//     filterOverlay.style.display = "none";
+//   } else {
+//     filterOverlay.style.display = "block";
+//   }
+// };
+
+// // sort
+
+// const sortOverlay = document.getElementById("sortOverlay");
+// const toggleSortBtn = document.getElementById("toggleSortBtn");
+// toggleSortBtn.onclick = function () {
+//   if (sortOverlay.style.display !== "none") {
+//     sortOverlay.style.display = "none";
+//   } else {
+//     sortOverlay.style.display = "block";
+//   }
+// };
+
+// // const addRecordOverlay = document.getElementById("addRecordOverlay");
+// const closeSortBtn = document.getElementById("closeSortBtn");
+// closeSortBtn.onclick = function () {
+//   if (sortOverlay.style.display === "none") {
+//     sortOverlay.style.display = "none";
+//   } else {
+//     sortOverlay.style.display = "block";
+//   }
+// };
+
+// const moreMenuActions = document.getElementById("moreMenuActions");
+// const toggleMoreMenu = document.getElementById("toggleMoreMenu");
+// toggleMoreMenu.onclick = function () {
+//   if (moreMenuActions.style.display !== "none") {
+//     moreMenuActions.style.display = "none";
+//   } else {
+//     moreMenuActions.style.display = "block";
+//   }
+// };
--- script.js
+++ script.js
@@ -1,45 +1,272 @@
-// // sidebar open close
-// let openBtn = document.querySelector(".fa-bars");
-// let closeBtn = document.querySelector(
-//   ".nav-container .sidebar-logo .close-btn"
-// );
-// let navigations = document.querySelector(".nav-container");
-
-// openBtn.addEventListener("click", function () {
-//   navigations.style.left = "0";
-// });
-
-const openBtn = document.getElementById("menuBar");
-openBtn.addEventListener("click", function () {
+const openMenuBar = document.getElementById("openMenuBar");
+openMenuBar.addEventListener("click", function () {
   document.getElementById("navs").style.left = "0";
 });
 
-const closeBtn = document.getElementById("closeBtn");
-closeBtn.addEventListener("click", function () {
+const closeMenuBar = document.getElementById("closeMenuBar");
+closeMenuBar.addEventListener("click", function () {
   document.getElementById("navs").style.left = "-100%";
 });
 
-const openAddOverlay = document.getElementById("addRecordBtn");
-openAddOverlay.addEventListener("click", function on() {
-  document.getElementById("addRecordOverlay").style.display = "block";
-});
+// const openFilterBtn = document.getElementById("openFilterBtn");
+// console.log(openFilterBtn);
+// openFilterBtn.addEventListener("click", function () {
+//   document.getElementById("filterOverlay");
+//   if (filterOverlay.style.display === "none") {
+//     filterOverlay.style.display = "block";
+//   } else {
+//     filterOverlay.style.display = "none";
+//   }
+// });
 
-const closeAddOverlay = document.getElementById("closeOverlayBtn");
-closeAddOverlay.addEventListener("click", function off() {
-  document.getElementById("addRecordOverlay").style.display = "none";
-});
+function toggleFilterBtn() {
+  var x = document.getElementById("filterOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
-const closeAddOverlay2 = document.getElementById("closeOverlayBtn2");
-closeAddOverlay2.addEventListener("click", function off() {
-  document.getElementById("addRecordOverlay").style.display = "none";
-});
+// const closeFilterBtn = document.getElementById("closeFilterBtn");
+// closeFilterBtn.addEventListener("click", function () {
+//   document.getElementById("filterOverlay").style.display = "none";
+// });
 
-const openFilterBtn = document.getElementById("openFilterBtn");
-openFilterBtn.addEventListener("click", function on() {
-  document.getElementById("filterOverlay").style.display = "block";
-});
+function closeFilterBtn() {
+  var x = document.getElementById("filterOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
-const closeFilterBtn = document.getElementById("closeFilterBtn");
-closeFilterBtn.addEventListener("click", function off() {
-  document.getElementById("filterOverlay").style.display = "none";
-});
+// const openSortBtn = document.getElementById("openSortBtn");
+// openSortBtn.addEventListener("click", function on() {
+//   document.getElementById("sortOverlay");
+//   if (sortOverlay.style.display === "none") {
+//     sortOverlay.style.display = "block";
+//   } else {
+//     sortOverlay.style.display = "none";
+//   }
+// });
+
+function toggleSortBtn() {
+  var x = document.getElementById("sortOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// const closeSortBtn = document.getElementById("closeSortBtn");
+// closeSortBtn.addEventListener("click", function off() {
+//   document.getElementById("sortOverlay").style.display = "none";
+// });
+
+function closeSortBtn() {
+  var x = document.getElementById("sortOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// function toggleMenuBtn() {
+//   var x = document.getElementById("moreMenuBtns");
+//   if (x.style.visibility === "hidden") {
+//     x.style.visibility = "visible";
+//   } else {
+//     x.style.visibility = "hidden";
+//   }
+// }
+
+// const toggleMoreMenu = document.getElementById("toggleMoreMenu");
+// toggleMoreMenu.addEventListener("click", function on() {
+//   document.getElementById("moreMenuActions");
+//   if (moreMenuActions.style.display === "none") {
+//     moreMenuActions.style.display = "block";
+//   } else {
+//     moreMenuActions.style.display = "none";
+//   }
+// });
+
+function toggleMoreMenu() {
+  var x = document.getElementById("moreMenuActions");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// Dashboard-header-add-record
+
+// const openAddOverlayDashboard = document.getElementById(
+//   "openAddOverlayDashboard"
+// );
+// openAddOverlayDashboard.addEventListener("click", function on() {
+//   document.getElementById("addRecordOverlayDashboard").style.display = "block";
+// });
+
+// const closeAddOverlayDB = document.getElementById("closeAddOverlayDB");
+// closeAddOverlayDB.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlayDB").style.display = "none";
+// });
+
+// const closeAddOverlay2DB = document.getElementById("closeAddOverlay2DB");
+// closeAddOverlay2DB.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlayDB").style.display = "none";
+// });
+
+// Records-header-add-record
+
+// const openAddOverlay2 = document.getElementById("openAddOverlayHeader");
+// openAddOverlay2.addEventListener("click", function on() {
+//   document.getElementById("addRecordOverlay").style.display = "block";
+// });
+
+// const openAddOverlay = document.getElementById("openAddOverlay");
+// openAddOverlay.addEventListener("click", function on() {
+//   document.getElementById("addRecordOverlay").style.display = "block";
+// });
+
+function openAddOverlay() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// const closeAddOverlay = document.getElementById("closeAddOverlay");
+// closeAddOverlay.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlay").style.display = "none";
+// });
+
+function closeAddOverlay() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// const closeAddOverlay2 = document.getElementById("closeAddOverlay2");
+// closeAddOverlay2.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlay").style.display = "none";
+// });
+
+function closeAddOverlay2() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// const openEditOverlay = document.getElementById("openEditOverlay");
+// openEditOverlay.addEventListener("click", function on() {
+//   document.getElementById("editRecordOverlay").style.display = "block";
+// });
+
+function openEditOverlay() {
+  var x = document.getElementById("editRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// const closeEditOverlay = document.getElementById("closeEditOverlay");
+// closeEditOverlay.addEventListener("click", function off() {
+//   document.getElementById("editRecordOverlay").style.display = "none";
+// });
+
+// function closeEditOverlay() {
+//   var x = document.getElementById("editRecordOverlay");
+//   if (x.style.display === "none") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+function closeEditOverlay() {
+  document.getElementById("editRecordOverlay").style.display = "none";
+}
+
+// const closeEditOverlay2 = document.getElementById("closeEditOverlay2");
+// closeEditOverlay2.addEventListener("click", function off() {
+//   document.getElementById("editRecordOverlay").style.display = "none";
+// });
+
+function closeEditOverlay2() {
+  var x = document.getElementById("editRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// const openAddOverlay2DB = document.getElementById("openAddOverlayHeader");
+// openAddOverlay2DB.addEventListener("click", function () {
+//   document.getElementById("addRecordOverlay").style.display = "block";
+// });
+
+function openAddOverlayHeader() {
+  var x = document.getElementById("addRecordOverlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// const closeAddOverlayDB = document.getElementById("closeAddOverlayDB");
+// closeAddOverlayDB.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlayDB").style.display = "none";
+// });
+
+// const closeAddOverlay2DB = document.getElementById("closeAddOverlay2DB");
+// closeAddOverlay2DB.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlayDB").style.display = "none";
+// });
+
+// const openAddOverlay3DB = document.getElementById("openAddOverlayHeader");
+// openAddOverlay3DB.addEventListener("click", function () {
+//   console.log(
+//     (document.getElementById("addRecordOverlay").style.display = "block")
+//   );
+// });
+
+// console.log(document.getElementById("openAddOverlayHeader"));
+// console.log(document.getElementById("openAddOverlayHeader"));
+
+// TO DO
+
+// Create js to close open overlays when another btn is toggled/clicked or when clicked outside
+// Make filter and sort buttons stay blue when theres active filters and sorts
+
+// const openAddOverlay22 = document.getElementById("openAddOverlayHeader2");
+// openAddOverlay22.addEventListener("click", function on() {
+//   document.getElementById("addRecordOverlay").style.display = "block";
+// });
+
+// const closeAddOverlay22 = document.getElementById("closeAddOverlay22");
+// closeAddOverlay22.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlay22").style.display = "none";
+// });
+
+// const closeAddOverlay222 = document.getElementById("closeAddOverlay222");
+// closeAddOverlay222.addEventListener("click", function off() {
+//   document.getElementById("addRecordOverlay").style.display = "none";
+// });
